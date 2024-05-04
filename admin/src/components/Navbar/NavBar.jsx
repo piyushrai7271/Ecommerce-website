3c1cbd39e6a9d505c1cd578b9e2b6f866f19fa03
piyushrai7271 <piyushrai7271@gmail.com>
2024-05-04 22:19:05 +0530

    fixing image Error

---
+++ admin/src/components/Navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+import navlogo from "../../assets/nav-logo.svg";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+        <img src="" alt="" className="nav-logo" />
+    </div>
+  )
+}
+
+export default NavBar
